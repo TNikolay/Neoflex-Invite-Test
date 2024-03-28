e661ae0ebe8436dda88c77e722cacc2e595094a9
Nikolay Tyushkov <begemot@begemotov.net>
2024-03-28 19:25:41 +0300

    feat: add basket paqe layout

---
+++ src/components/BasketCard.tsx
@@ -0,0 +1,52 @@
+import { IProduct } from '@/Database/types'
+import { Button } from '@/components/ui/button'
+
+interface IProps {
+  readonly data: IProduct
+}
+
+export const BasketCard = ({ data }: IProps) => {
+  const { title, price } = data
+  const img = `./ProductsImages/${data.img}.png`
+
+  return (
+    <div className='hover:border-1 flex h-[218px] w-[633px] items-center rounded-[30px] border bg-white px-5 py-2'>
+      <div className='flex-col'>
+        <img src={img} className='h-[136px] w-[148px]' alt={title} />
+
+        <div className='mt-5 flex w-full items-center justify-between'>
+          <Button variant='ghost' size='icon'>
+            <svg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <circle cx='15' cy='15' r='15' fill='#FFCE7F' />
+              <path d='M8 14H22V16H8V14Z' fill='white' />
+            </svg>
+          </Button>
+
+          <span className='text-lg font-[600]'>1</span>
+
+          <Button variant='ghost' size='icon'>
+            <svg width='31' height='30' viewBox='0 0 31 30' fill='none' xmlns='http://www.w3.org/2000/svg'>
+              <circle cx='15.2056' cy='15' r='15' fill='#FFCE7F' />
+              <path d='M14.2056 14V8H16.2056V14H22.2056V16H16.2056V22H14.2056V16H8.20557V14H14.2056Z' fill='white' />
+            </svg>
+          </Button>
+        </div>
+      </div>
+
+      <div className='ml-6 flex-grow flex-col pb-6'>
+        <h4 className='mb-2 text-lg font-[500]'>{title}</h4>
+        <span className='text-sm font-[500] text-[#838383]'>{price} ₽</span>
+      </div>
+
+      <div className='flex h-full flex-col justify-between'>
+        <Button variant='ghost' size='icon' className='self-end'>
+          <svg width='21' height='17' viewBox='0 0 21 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
+            <path d='M15.8848 3.4H20.8667V5.1H18.874V16.15C18.874 16.3754 18.769 16.5916 18.5821 16.751C18.3953 16.9104 18.1418 17 17.8776 17H3.92813C3.66387 17 3.41044 16.9104 3.22358 16.751C3.03672 16.5916 2.93174 16.3754 2.93174 16.15V5.1H0.938965V3.4H5.92091V0.85C5.92091 0.624566 6.02589 0.408365 6.21275 0.248959C6.3996 0.0895533 6.65304 0 6.9173 0H14.8884C15.1527 0 15.4061 0.0895533 15.593 0.248959C15.7798 0.408365 15.8848 0.624566 15.8848 0.85V3.4ZM16.8812 5.1H4.92452V15.3H16.8812V5.1ZM12.3117 10.2L14.0734 11.7028L12.6645 12.9047L10.9029 11.4019L9.14124 12.9047L7.73234 11.7028L9.49396 10.2L7.73234 8.6972L9.14124 7.4953L10.9029 8.9981L12.6645 7.4953L14.0734 8.6972L12.3117 10.2ZM7.91369 1.7V3.4H13.892V1.7H7.91369Z' fill='#DF6464' />
+          </svg>
+        </Button>
+
+        <div className='mb-2 text-sm font-[600]'>{price} ₽</div>
+      </div>
+    </div>
+  )
+}
--- src/pages/BasketPage.tsx
+++ src/pages/BasketPage.tsx
@@ -1,3 +1,30 @@
+import { db } from '@/Database/mongodb'
+import { BasketCard } from '@/components/BasketCard'
+import { Button } from '@/components/ui/button'
+
 export const BasketPage = () => {
-  return <div>BasketPage</div>
+  const products = db[1].products
+  const total = 2927
+  return (
+    <div className='py-2 pl-3'>
+      <h2 className='py-5 text-xl font-[600]'>Корзина</h2>
+
+      <div className='flex justify-between'>
+        <div className='flex flex-col gap-6'>
+          {products.map((product) => (
+            <BasketCard data={product} />
+          ))}
+        </div>
+
+        <div className='flex h-[114px] w-[348px] flex-shrink-0 flex-col '>
+          <div className='flex justify-between rounded-3xl bg-white px-8 pb-10 pt-4 font-[600]'>
+            <div>ИТОГО</div>
+            <div>₽ {total}</div>
+          </div>
+
+          <Button className='mt-[-2rem] w-full rounded-2xl py-8 text-lg font-[600]'>Перейти к оформлению</Button>
+        </div>
+      </div>
+    </div>
+  )
 }
