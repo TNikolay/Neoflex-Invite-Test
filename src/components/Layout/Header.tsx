a39b02512a3dae9947a78d7c563eeae82b823b7f
Nikolay Tyushkov <begemot@begemotov.net>
2024-03-28 11:09:30 +0300

    feat: add shadcn/ui, create header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,11 +1,23 @@
+import { BasketIcon } from '@/components/ui/BasketIcon'
+import { FavoriteIcon } from '@/components/ui/FavoriteIcon.tsx'
 import { Link } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <header>
-      <Link to='/'>Home</Link> ::
-      <Link to='/basket'> Basket</Link> ::
-      <Link to='/404'> 404</Link>
+    <header className='flex justify-between py-8'>
+      <Link to='/' className='text-xl font-bold hover:opacity-60'>
+        QPICK
+      </Link>
+
+      <div className='flex gap-10'>
+        <Link to='/favorites'>
+          <FavoriteIcon />
+        </Link>
+
+        <Link to='/basket'>
+          <BasketIcon />
+        </Link>
+      </div>
     </header>
   )
 }
