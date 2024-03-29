9c56816dff27ea207900dc154526c0e9f8b8c361
Nikolay Tyushkov <begemot@begemotov.net>
2024-03-29 12:21:58 +0300

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/neoflex-invite-test/',
   plugins: [react()],
   resolve: {
     alias: {
