821182c307f01f5973f4666fd13c73fa1b35d8c6
Nikolay Tyushkov <begemot@begemotov.net>
2024-04-02 17:26:06 +0300

    feat: save favorites and basket items locally

--- src/store/basketStore.tsx
+++ src/store/basketStore.tsx
@@ -1,5 +1,6 @@
 import { IBasketItem } from '@/Database/types'
 import { create } from 'zustand'
+import { StorageValue, persist } from 'zustand/middleware'
 
 type State = {
   basket: IBasketItem
@@ -12,35 +13,67 @@ type Action = {
   deleteProduct: (id: string) => void
 }
 
-export const useBasketStore = create<State & Action>((set) => ({
-  basket: new Map<string, number>(),
-  count: 0,
+export const useBasketStore = create<State & Action>()(
+  persist(
+    (set) => ({
+      basket: new Map<string, number>(),
+      count: 0,
 
-  addProduct: (id) => {
-    set((state) => {
-      const basket = new Map(state.basket)
-      basket.set(id, (basket.get(id) || 0) + 1)
-      return { basket, count: state.count + 1 }
-    })
-  },
-  decrementProduct: (id) => {
-    set((state) => {
-      const basket = new Map(state.basket)
-      const count = basket.get(id)
+      addProduct: (id) => {
+        set((state) => {
+          const basket = new Map(state.basket)
+          basket.set(id, (basket.get(id) || 0) + 1)
+          return { basket, count: state.count + 1 }
+        })
+      },
+      decrementProduct: (id) => {
+        set((state) => {
+          const basket = new Map(state.basket)
+          const count = basket.get(id)
 
-      if (!count) return {}
+          if (!count) return {}
 
-      if (count > 1) basket.set(id, count - 1)
-      else basket.delete(id)
+          if (count > 1) basket.set(id, count - 1)
+          else basket.delete(id)
 
-      return { basket, count: state.count - 1 }
-    })
-  },
+          return { basket, count: state.count - 1 }
+        })
+      },
 
-  deleteProduct: (id) => {
-    set((state) => {
-      const basket = new Map(state.basket)
-      return basket.delete(id) ? { basket, count: state.count - 1 } : {}
-    })
-  },
-}))
+      deleteProduct: (id) => {
+        set((state) => {
+          const basket = new Map(state.basket)
+          return basket.delete(id) ? { basket, count: state.count - 1 } : {}
+        })
+      },
+    }),
+    {
+      name: 'TN_NIT_Basket',
+      // https://github.com/pmndrs/zustand/blob/main/docs/integrations/persisting-store-data.md#how-do-i-use-it-with-map-and-set
+      storage: {
+        getItem: (name) => {
+          const str = localStorage.getItem(name)
+          if (!str) return null
+          const { state } = JSON.parse(str)
+          return {
+            state: {
+              ...state,
+              basket: new Map(state.basket),
+            },
+          }
+        },
+        setItem: (name, newValue: StorageValue<State>) => {
+          // functions cannot be JSON encoded
+          const str = JSON.stringify({
+            state: {
+              ...newValue.state,
+              basket: Array.from(newValue.state.basket.entries()),
+            },
+          })
+          localStorage.setItem(name, str)
+        },
+        removeItem: (name) => localStorage.removeItem(name),
+      },
+    }
+  )
+)
--- src/store/favoritesStore.tsx
+++ src/store/favoritesStore.tsx
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 type State = {
   favorites: string[]
@@ -8,10 +9,17 @@ type Action = {
   toggleFavorite: (id: string) => void
 }
 
-export const useFavoritesStore = create<State & Action>((set) => ({
-  favorites: [],
+export const useFavoritesStore = create<State & Action>()(
+  persist(
+    (set) => ({
+      favorites: [],
 
-  toggleFavorite: (id) => {
-    set((state) => (state.favorites.includes(id) ? { favorites: state.favorites.filter((v) => v !== id) } : { favorites: [id, ...state.favorites] }))
-  },
-}))
+      toggleFavorite: (id) => {
+        set((state) => (state.favorites.includes(id) ? { favorites: state.favorites.filter((v) => v !== id) } : { favorites: [id, ...state.favorites] }))
+      },
+    }),
+    {
+      name: 'TN_NIT_favorites',
+    }
+  )
+)
